a8919af5d17c2b350ae5d2d4f56af96e7d46000a
Mihai Pocorschi <mihai@pocorschi.com>
2021-11-29 12:15:18 +0200

    working mobile

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { AppProvider } from '../state/AppContext';
 
@@ -8,11 +9,16 @@ const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AppProvider>
-        <Component {...pageProps} />
-      </AppProvider>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <AppProvider>
+          <Component {...pageProps} />
+        </AppProvider>
+      </QueryClientProvider>
+    </>
   );
 }
 
