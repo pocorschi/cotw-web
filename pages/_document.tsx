7d85348e3d584c950ea00c41b1c15455833688f9
Mihai Pocorschi <mihai@pocorschi.com>
2021-12-06 20:13:11 +0200

    added GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,19 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;800&display=swap"
             rel="stylesheet"
           />
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=G-CQ2JRBJ8CK}`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CQ2JRBJ8CK', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
